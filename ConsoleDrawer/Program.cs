b6ac4f29a4fd7139604de9360bb5899568ccdce1
mh554 <2005.barta.marko@gmail.com>
2024-10-03 16:16:30 +0200

    Add files via upload

--- ConsoleDrawer/Program.cs
+++ ConsoleDrawer/Program.cs
@@ -6,43 +6,57 @@ class Program
     static int cursorX = 0;
     static int cursorY = 0;
 
+    static bool running = true;
+
     static ConsoleColor currentColor = ConsoleColor.White;
+    static readonly Dictionary<(int, int), (char, ConsoleColor)> drawing = new();
 
     static void Main()
     {
         Console.Clear();
         Console.CursorVisible = true;
+        LoadMenu();
 
-        while (true)
+        while (!running)
         {
-            if (Console.KeyAvailable)
-            {
-                var key = Console.ReadKey(true).Key;
+            LoadMenu();
+        }
 
-                switch (key)
-                {
-                    case ConsoleKey.UpArrow:
-                        MoveCursor(0, -1);
-                        break;
-                    case ConsoleKey.DownArrow:
-                        MoveCursor(0, 1);
-                        break;
-                    case ConsoleKey.LeftArrow:
-                        MoveCursor(-1, 0);
-                        break;
-                    case ConsoleKey.RightArrow:
-                        MoveCursor(1, 0);
-                        break;
-                    case ConsoleKey.Spacebar:
-                        PlaceMarker();
-                        break;
-                    case ConsoleKey.Backspace:
-                        DeleteMarker();
-                        break;
-                    default:
-                        ChangeColor(key);
-                        break;
-                }
+        while (running)
+        {
+            var key = Console.ReadKey(true).Key;
+
+            switch (key)
+            {
+                case ConsoleKey.UpArrow:
+                    MoveCursor(0, -1);
+                    break;
+                case ConsoleKey.DownArrow:
+                    MoveCursor(0, 1);
+                    break;
+                case ConsoleKey.LeftArrow:
+                    MoveCursor(-1, 0);
+                    break;
+                case ConsoleKey.RightArrow:
+                    MoveCursor(1, 0);
+                    break;
+                case ConsoleKey.Spacebar:
+                    PlaceMarker();
+                    break;
+                case ConsoleKey.Backspace:
+                    DeleteMarker();
+                    break;
+                case ConsoleKey.S:
+                    SaveDrawing("drawing.txt");
+                    break;
+                case ConsoleKey.L:
+                    LoadDrawing("drawing.txt");
+                    break;
+                case ConsoleKey.Escape:
+                    break;
+                default:
+                    ChangeColor(key);
+                    break;
             }
         }
     }
@@ -53,19 +67,21 @@ class Program
         cursorY = Math.Max(0, Math.Min(Console.WindowHeight - 1, cursorY + deltaY));
 
         Console.SetCursorPosition(cursorX, cursorY);
-
     }
 
     static void PlaceMarker()
     {
+        Console.ForegroundColor = currentColor;
         Console.Write("█");
+        drawing[(cursorX, cursorY)] = ('█', currentColor);
+        MoveCursor(1, 0); 
     }
 
     static void DeleteMarker()
     {
         Console.Write(" ");
+        drawing.Remove((cursorX, cursorY));
     }
-
 
     static void ChangeColor(ConsoleKey key)
     {
@@ -100,5 +116,80 @@ class Program
                 break;
         }
         Console.ForegroundColor = currentColor;
+    }
+
+    static void SaveDrawing(string filePath)
+    {
+        using (var writer = new StreamWriter(filePath))
+        {
+            foreach (var entry in drawing)
+            {
+                var (position, (character, color)) = entry;
+                writer.WriteLine($"{position.Item1},{position.Item2},{character},{(int)color}");
+            }
+        }
+    }
+
+    static void LoadDrawing(string filePath)
+    {
+        if (!File.Exists(filePath)) return;
+
+        drawing.Clear();
+        Console.Clear();
+
+        using (var reader = new StreamReader(filePath))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                var parts = line.Split(',');
+                var x = int.Parse(parts[0]);
+                var y = int.Parse(parts[1]);
+                var character = parts[2][0];
+                var color = (ConsoleColor)int.Parse(parts[3]);
+
+                drawing[(x, y)] = (character, color);
+                Console.SetCursorPosition(x, y);
+                Console.ForegroundColor = color;
+                Console.Write(character);
+            }
+        }
+        Console.SetCursorPosition(cursorX, cursorY);
+        Console.ForegroundColor = currentColor;
+    }
+
+    static void LoadMenu()
+    {
+        if (running == true)
+        {
+            Console.Clear();
+            Console.WriteLine("Drawer thing");
+            Console.WriteLine("Press L to load a file");
+            Console.WriteLine("Press S to save to a file");
+            Console.WriteLine("Press C to start drawing");
+        }
+        running = false;
+
+        var key = Console.ReadKey(true).Key;
+
+        switch (key)
+        {
+            case ConsoleKey.L:
+                break;
+            case ConsoleKey.S:
+                break;
+            case ConsoleKey.C: 
+                break;
+            default:
+                break;
+        }
+
+        if (true)
+        {
+            
+        }
+
+
+
     }
 }
